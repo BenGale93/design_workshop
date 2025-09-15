0654d5a3af8730166d4cca6ed099073597ffdd70
Ben Gale <bengale2007@googlemail.com>
2025-09-15 20:48:24 +0100

    Strategy pattern

--- src/chunking.py
+++ src/chunking.py
@@ -1,4 +1,4 @@
-"""Now let's make our return object more useful."""
+"""Now let's use the strategy pattern for the file type specific logic."""
 
 import re
 import typing as t
@@ -70,7 +70,77 @@ class ChunkedDocument:
         return sections[0]
 
 
-def chunk_document(document: str, doc_type: t.Literal["markdown", "latex"]) -> ChunkedDocument:
+class Chunker(t.Protocol):
+    def chunk_document(self, document: str) -> ChunkedDocument: ...
+
+
+class MarkdownChunker:
+    h1_pattern = r"^# (.+)$"
+    code_block_pattern = r"```"
+
+    def chunk_document(self, document: str) -> ChunkedDocument:
+        chunked_document = ChunkedDocument()
+        current_heading: str | None = None
+        current_content: list[str] = []
+        in_code_block = False
+
+        for raw_line in document.splitlines():
+            line = raw_line.strip()
+            if re.match(self.code_block_pattern, line):
+                in_code_block = not in_code_block
+
+            if in_code_block:
+                current_content.append(line)
+                continue
+
+            h1_match = re.match(self.h1_pattern, line)
+
+            if h1_match:
+                if current_content:
+                    chunked_document.add_section(current_heading, current_content)
+
+                current_heading = h1_match.group(1)
+                current_content = []
+            else:
+                current_content.append(line)
+
+        chunked_document.add_section(current_heading, current_content)
+
+        return chunked_document
+
+
+class LatexChunker:
+    h1_pattern = r"^\\section\{(.+)\}"
+
+    def chunk_document(self, document: str) -> ChunkedDocument:
+        chunked_document = ChunkedDocument()
+        current_heading: str | None = None
+        current_content: list[str] = []
+
+        for raw_line in document.splitlines():
+            line = raw_line.strip()
+            h1_match = re.match(self.h1_pattern, line)
+            if h1_match:
+                if current_content:
+                    chunked_document.add_section(current_heading, current_content)
+
+                current_heading = h1_match.group(1)
+                current_content = []
+            else:
+                current_content.append(line)
+
+        chunked_document.add_section(current_heading, current_content)
+
+        return chunked_document
+
+
+CHUNKERS: dict[str, Chunker] = {
+    "markdown": MarkdownChunker(),
+    "latex": LatexChunker(),
+}
+
+
+def chunk_document(document: str, doc_type: str) -> ChunkedDocument:
     """Chunks a document into sections and subsections, removing any comments.
 
     Args:
@@ -80,38 +150,10 @@ def chunk_document(document: str, doc_type: t.Literal["markdown", "latex"]) -> C
     Returns:
         Document instance containing the sections and sub-sections.
     """
-    if doc_type == "markdown":
-        h1_pattern = r"^# (.+)$"
-        code_block_pattern = r"```"
-    else:
-        h1_pattern = r"^\\section\{(.+)\}"
-        code_block_pattern = None
+    try:
+        doc_chunker = CHUNKERS[doc_type]
+    except KeyError:
+        msg = f"{doc_type} not supported"
+        raise ValueError(msg) from None
 
-    chunked_document = ChunkedDocument()
-    current_heading: str | None = None
-    current_content: list[str] = []
-    in_code_block = False
-
-    for raw_line in document.splitlines():
-        line = raw_line.strip()
-        if code_block_pattern and re.match(code_block_pattern, line):
-            in_code_block = not in_code_block
-
-        if in_code_block:
-            current_content.append(line)
-            continue
-
-        h1_match = re.match(h1_pattern, line)
-
-        if h1_match:
-            if current_content:
-                chunked_document.add_section(current_heading, current_content)
-
-            current_heading = h1_match.group(1)
-            current_content = []
-        else:
-            current_content.append(line)
-
-    chunked_document.add_section(current_heading, current_content)
-
-    return chunked_document
+    return doc_chunker.chunk_document(document)
