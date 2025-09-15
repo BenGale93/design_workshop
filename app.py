926afc5d9d28119f80657edb84a1709f8699f262
Ben Gale <bengale2007@googlemail.com>
2025-09-15 20:41:47 +0100

    Dependency injection

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from chunking import chunk_document, download_document_from_github
+from chunking import RawGitHubDownloader, chunk_document
 
-raw_doc = download_document_from_github("BenGale93/cli-diary/refs/heads/master/README.md")
+downloader = RawGitHubDownloader()
+
+raw_doc = downloader.get_document("BenGale93/cli-diary/refs/heads/master/README.md")
 doc = chunk_document(raw_doc, "markdown")
 
 section = next(s for s in doc if s["title"] == "cli-diary")
